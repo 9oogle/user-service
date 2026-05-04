47c7562a5038050a8ceb1c923e5a972be6075b28
sionkim0126 <kimsion000@naver.com>
2026-05-04 19:00:20 +0900

    feat : SecurityConfig 추가

---
+++ src/main/java/com/goggles/user_service/common/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.goggles.user_service.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()  // 회원가입
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()     // 로그인
+                        .requestMatchers("/internal/v1/user/**").permitAll()             // 내부 API
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
