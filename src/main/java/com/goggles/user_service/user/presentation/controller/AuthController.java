994f8331e0844f889671da5b338f2d51cd837261
sionkim0126 <kimsion000@naver.com>
2026-05-04 18:59:30 +0900

    fix : api 수정

--- src/main/java/com/goggles/user_service/user/presentation/controller/AuthController.java
+++ src/main/java/com/goggles/user_service/user/presentation/controller/AuthController.java
@@ -9,15 +9,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public ApiResponse<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
         LoginResult result = authService.login(request.toServiceRequest());
         return ApiResponse.success(LoginResponse.from(result));
