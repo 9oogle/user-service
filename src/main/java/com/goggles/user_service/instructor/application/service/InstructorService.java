1aba59e00e99c0fcc4b93d2d6b72dfc36b838d54
sionkim0126 <kimsion000@naver.com>
2026-05-08 12:28:07 +0900

    fix : save() 반환된 영속 엔티티를 Result 변환에 사용

--- src/main/java/com/goggles/user_service/instructor/application/service/InstructorService.java
+++ src/main/java/com/goggles/user_service/instructor/application/service/InstructorService.java
@@ -4,9 +4,7 @@ import com.goggles.user_service.instructor.application.dto.InstructorApplyComman
 import com.goggles.user_service.instructor.application.dto.InstructorApplyResult;
 import com.goggles.user_service.instructor.domain.entity.Instructor;
 import com.goggles.user_service.instructor.domain.exception.DuplicateInstructorException;
-import com.goggles.user_service.instructor.domain.exception.InstructorForbiddenException;
 import com.goggles.user_service.instructor.domain.repository.InstructorRepository;
-import com.goggles.user_service.user.domain.entity.Role;
 import com.goggles.user_service.user.domain.entity.User;
 import com.goggles.user_service.user.domain.exception.UserNotFoundException;
 import com.goggles.user_service.user.domain.repository.UserRepository;
@@ -28,15 +26,14 @@ public class InstructorService {
             .findById(command.requestId())
             .orElseThrow(() -> new UserNotFoundException("user.notfound"));
 
-    if (requester.getRole() != Role.STUDENT) {
-      throw new InstructorForbiddenException("instructor.exception.apply.forbidden");
-    }
+    requester.validateCanApplyAsInstructor();
+
     if (instructorRepository.existsByUserId(command.requestId())) {
       throw new DuplicateInstructorException("instructor.exception.apply.duplicate");
     }
 
     Instructor instructor = command.toInstructor();
-    instructorRepository.save(instructor);
-    return InstructorApplyResult.from(instructor);
+    Instructor saved = instructorRepository.save(instructor);
+    return InstructorApplyResult.from(saved);
   }
 }
