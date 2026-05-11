ff8f1cc9a22dc18f40ac5c983970a968aaa4f4cf
sionkim0126 <kimsion000@naver.com>
2026-05-11 11:45:28 +0900

    refactor : findById 구현

--- src/main/java/com/goggles/user_service/instructor/infrastructure/repository/JpaInstructorRepository.java
+++ src/main/java/com/goggles/user_service/instructor/infrastructure/repository/JpaInstructorRepository.java
@@ -1,6 +1,7 @@
 package com.goggles.user_service.instructor.infrastructure.repository;
 
 import com.goggles.user_service.instructor.domain.entity.Instructor;
+import com.goggles.user_service.instructor.domain.entity.InstructorId;
 import com.goggles.user_service.instructor.domain.repository.InstructorRepository;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,7 +16,7 @@ public class JpaInstructorRepository implements InstructorRepository {
 
   @Override
   public Optional<Instructor> findById(UUID id) {
-    return Optional.empty();
+    return repository.findById(InstructorId.of(id));
   }
 
   @Override
