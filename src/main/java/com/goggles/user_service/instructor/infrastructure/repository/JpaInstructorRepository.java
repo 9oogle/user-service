c80970466356b0e991ea0ee6f680024bf6814d78
sionkim0126 <kimsion000@naver.com>
2026-05-12 00:46:47 +0900

    feat : 강사 승인 목록조회 repository 인터페이스 및 구현체 구현

--- src/main/java/com/goggles/user_service/instructor/infrastructure/repository/JpaInstructorRepository.java
+++ src/main/java/com/goggles/user_service/instructor/infrastructure/repository/JpaInstructorRepository.java
@@ -2,10 +2,13 @@ package com.goggles.user_service.instructor.infrastructure.repository;
 
 import com.goggles.user_service.instructor.domain.entity.Instructor;
 import com.goggles.user_service.instructor.domain.entity.InstructorId;
+import com.goggles.user_service.instructor.domain.entity.InstructorStatus;
 import com.goggles.user_service.instructor.domain.repository.InstructorRepository;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -27,5 +30,15 @@ public class JpaInstructorRepository implements InstructorRepository {
   @Override
   public Instructor save(Instructor instructor) {
     return repository.save(instructor);
+  }
+
+  @Override
+  public Page<Instructor> findAllByStatus(InstructorStatus status, Pageable pageable) {
+    return repository.findAllByStatus(status, pageable);
+  }
+
+  @Override
+  public Page<Instructor> findAll(Pageable pageable) {
+    return repository.findAll(pageable);
   }
 }
